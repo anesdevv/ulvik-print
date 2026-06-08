d1b7d5a00d3e2ff13301a48818bef67ef5c55fc0
Ulvik Print Admin <admin@ulvikprint.com>
2026-06-08 23:19:19 +0100

    fix: update Instagram link to ulvik_print_net

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const WHATSAPP_RAW_NUMBER = '0561396333';
 export const WHATSAPP_NUMBER = WHATSAPP_RAW_NUMBER.replace(/^0/, '213');
-export const INSTAGRAM_URL = 'https://www.instagram.com/ulvic_print_net';
+export const INSTAGRAM_URL = 'https://www.instagram.com/ulvik_print_net/';
 export const WHATSAPP_DEFAULT_MSG = 'Bonjour, je voudrais personnaliser un t-shirt.';
